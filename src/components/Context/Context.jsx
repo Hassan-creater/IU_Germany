5842833b01c5b3181144d282aef6bfe6ffdb7eb1
Hassan-dev <034217602ab@gmail.com>
2024-10-01 11:17:12 -0700

    Update Context.jsx

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -1,17 +1,1 @@
-import { createContext, useState } from "react";
 
-
-
-
-
-export const MyContext = createContext();
-
-export const MyProvider = ({ children }) => {
-    const [modal, showModal] = useState(false);
-
-    return (
-        <MyContext.Provider value={{ modal, showModal }}>
-            {children}
-        </MyContext.Provider>
-    );
-};
